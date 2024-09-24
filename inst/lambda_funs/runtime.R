8f09387dd3147aff3d300437cd410299c4a38284
sebsilas <sebsilas@gmail.com>
2024-09-24 19:04:40 +0000

    Disable pool for some Lambdas

--- inst/lambda_funs/runtime.R
+++ inst/lambda_funs/runtime.R
@@ -38,7 +38,13 @@ item_banks_table_static <- tibble::tibble(
 
 
 # ------- DATABASE CONNECTION -------------------- #
-db_con <- musicassessr_con()
+
+if(Sys.getenv("DISABLE_POOL") == "TRUE") {
+  db_con <- musicassessr_con(pool = FALSE)
+} else {
+  db_con <- musicassessr_con()
+}
+
 on.exit({
   db_disconnect(db_con)
   logging::loginfo("...disconnected from DB")
