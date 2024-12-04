18e7bc28c6313bca9437b1e136204707b483b1e0
sebsilas <sebsilas@gmail.com>
2024-12-04 11:50:06 +0000

    Test log jiao

--- R/lambda_select_items.R
+++ R/lambda_select_items.R
@@ -49,7 +49,7 @@ select_items <- function(user_id) {
 
   logging::loginfo("Inside select_items function")
 
-  logging::loginfo("Test log lasdasd!")
+  logging::loginfo("Test log jiaosdu9as!")
 
 
   response <- tryCatch({
