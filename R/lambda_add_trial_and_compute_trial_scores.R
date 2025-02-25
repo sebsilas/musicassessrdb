04680a72fbe857e4fccda42f2c2338ec2be56dc6
sebsilas <sebsilas@gmail.com>
2025-02-25 14:30:02 +0000

    Logging

--- R/lambda_add_trial_and_compute_trial_scores.R
+++ R/lambda_add_trial_and_compute_trial_scores.R
@@ -81,6 +81,8 @@ add_trial_and_compute_trial_scores <- function(Records) {
     # Handle immediate feedback
     handle_quick_feedback(feedback, feedback_type, stimuli, stimuli_durations, stim_length, res, audio_file)
 
+    logging::loginfo("handle_quick_feedback complete")
+    logging::loginfo("attempt: %s", attempt)
 
     # Append trial info
     trial_id <- db_append_trials(
