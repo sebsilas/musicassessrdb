300ac232479b6690b7a4e2e86c0fbb7a908ef41b
sebsilas <sebsilas@gmail.com>
2025-04-08 21:54:11 +0000

    Updates

--- R/lambda_add_trial_and_compute_trial_scores.R
+++ R/lambda_add_trial_and_compute_trial_scores.R
@@ -72,13 +72,13 @@ add_trial_and_compute_trial_scores <- function(Records) {
     logging::loginfo("onset again: %s", onset)
 
     if(onset) {
+
       res <- res %>%
         dplyr::mutate(onset = as.numeric(onset),
                       dur = c(diff(onset), 0.5),
                       freq = NA,
                       note = NA) %>%
-        dplyr::relocate(freq, dur, onset, note) %>%
-        itembankr::produce_extra_melodic_features()
+        dplyr::relocate(freq, dur, onset, note)
 
     } else {
       res <- res %>%
