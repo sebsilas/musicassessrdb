d145196afb52f877328c8a1348ecc74dd71bb984
sebsilas <sebsilas@gmail.com>
2025-05-08 11:49:17 +0000

    Update sample in ntiles wrapper

--- R/lambda_sample_in_ntiles_wjd.R
+++ R/lambda_sample_in_ntiles_wjd.R
@@ -8,14 +8,36 @@
 # }'
 
 
-sample_in_ntiles_wjd <- function(num_items = 12L) {
+# t <- sample_in_ntiles_wjd()
+
+sample_in_ntiles_wjd <- function(num_item_arrhythmic = 8L,
+                                 num_items_rhythmic = 8L) {
 
   tryCatch({
 
-    sample <- musicassessr::sample_in_ntiles_wrapper(musicassessr::pbet_hmtm_2024_item_bank,
-                                                     num_items = num_items,
-                                                     col_name = "SAA_arrhythmic_difficulty_percentile",
-                                                     n = 4)
+
+
+    sample_arrhythmic <- musicassessr::sample_in_ntiles_wrapper(musicassessr::pbet_hmtm_2024_item_bank,
+                                                                num_items = num_item_arrhythmic,
+                                                                ntile_sampling_upper_bound = 4L,
+                                                                col_name = "arrhythmic_difficulty_percentile",
+                                                                n = 4) %>%
+      dplyr::mutate(rhythmic = FALSE) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(durations = paste0(rep(0.5, N), collapse = ",") ) %>%
+      dplyr::ungroup()
+
+    sample_rhythmic <- musicassessr::sample_in_ntiles_wrapper(musicassessr::pbet_hmtm_2024_item_bank,
+                                                              num_items = num_items_rhythmic,
+                                                              ntile_sampling_upper_bound = 4L,
+                                                              col_name = "rhythmic_difficulty_percentile",
+                                                              n = 4) %>%
+      dplyr::mutate(rhythmic = TRUE)
+
+
+    sample <- rbind(sample_arrhythmic, sample_rhythmic) %>%
+      dplyr::slice_sample(n = nrow(.))
+
 
     list(
       status = 200,
