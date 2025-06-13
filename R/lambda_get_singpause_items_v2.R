902ab63b5d49cb118d297cebcc1dddfbfe59c2b5
sebsilas <sebsilas@gmail.com>
2025-06-13 11:16:26 +0000

    U

--- R/lambda_get_singpause_items_v2.R
+++ R/lambda_get_singpause_items_v2.R
@@ -2,6 +2,8 @@
 
 # db_con <- musicassessr_con()
 
+# t <- get_singpause_items_v2(189)
+
 get_singpause_items_v2 <- function(user_id = NULL) {
 
 
@@ -20,7 +22,15 @@ get_singpause_items_v2 <- function(user_id = NULL) {
 
   trials <- compile_item_trials(db_con, user_id = user_id, add_trial_scores = TRUE) %>%
     dplyr::filter(grepl("singpause_2025", item_id)) %>%
-    dplyr::mutate(Date = lubridate::as_datetime(trial_time_completed)) %>%
+    dplyr::mutate(Date = lubridate::as_datetime(trial_time_completed))
+
+  # For cases where no trials yet
+  if(!"opti3" %in% names(trials)) {
+    trials <- trials %>%
+      dplyr::mutate(opti3 = 0)
+  }
+
+  trials <- trials %>%
     dplyr::rename(score = opti3) %>%
     dplyr::select(Date, item_id, score) %>%
     dplyr::group_by(item_id) %>%
