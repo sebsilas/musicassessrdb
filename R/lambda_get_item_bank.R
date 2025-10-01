22c0d703c172b7c60109ccf444ad2b9917baff8e
sebsilas <sebsilas@gmail.com>
2025-10-01 14:51:30 +0000

    Add optional N filter

--- R/lambda_get_item_bank.R
+++ R/lambda_get_item_bank.R
@@ -2,6 +2,12 @@
 
 # db_con <- musicassessr_con()
 
+# curl -X POST 'https://api.dev.songbird.training/get-item-bank' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#   "item_bank": "item_bank_so_so_index"
+# }'
+
 # t <- get_item_bank("item_bank_so_so_index")
 
 get_item_bank <- function(item_bank = NULL) {
@@ -30,7 +36,7 @@ get_item_bank <- function(item_bank = NULL) {
     list(
       status = 400,
       message = "Something went wrong!",
-      sample = NA
+      item_bank = NA
     )
 
   })
